fddb2ae22a9005111030ff0c8dd3903ac4135418
田中祐希 <tanakayuuki@tanakayuukinoMacBook-Air.local>
2023-10-12 11:35:31 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,14 +1,27 @@
 class Vec3 {
-    constructor(x,y,z)
-    {
-    this.x = x
-    this.y = y
-    this.z = z
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
-add(v){
-this.x += v.x;
-this.y += v.y;
-this.z += v.z;
-return this;
-}
+
+    add(v) {
+        this.x += v.x;
+        this.y += v.y;
+        this.z += v.z;
+        return this;
+    }
+
+    max() {
+        return Math.max(this.x, this.y, this.z);
+    }
+
+    min() {
+        return Math.min(this.x, this.y, this.z);
+    }
+
+    mid() {
+        const values = [this.x, this.y, this.z].sort((a, b) => a - b);
+        return values[1]; // 中央の値を返す
+    }
 }
