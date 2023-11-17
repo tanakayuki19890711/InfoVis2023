1012c7103e3c85e27dfbc347c7bd1c1736d25bde
田中祐希 <tanakayuuki@tanakayuukinoMacBook-Air.local>
2023-11-17 14:40:57 +0900

    task2

---
+++ W06/task2.js
@@ -0,0 +1,131 @@
+// X軸ラベルの追加
+self.svg.append("text")             
+.attr("transform",
+      "translate(" + (self.inner_width/2 + self.config.margin.left) + " ," + 
+                     (self.inner_height + self.config.margin.top + 20) + ")")
+.style("text-anchor", "middle")
+.text("X軸のラベル"); // ここにX軸のラベルを設定
+
+// Y軸ラベルの追加
+self.svg.append("text")
+.attr("transform", "rotate(-90)")
+.attr("y", 0 - self.config.margin.left)
+.attr("x",0 - (self.inner_height / 2))
+.attr("dy", "1em")
+.style("text-anchor", "middle")
+.text("Y軸のラベル"); // ここにY軸のラベルを設定
+
+
+
+d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2021/W04/data.csv")
+    .then( data => {
+        data.forEach( d => { d.x = +d.x; d.y = +d.y; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 256,
+            height: 256,
+            margin: {top:10, right:10, bottom:20, left:10}
+        };
+
+        const scatter_plot = new ScatterPlot( config, data );
+        scatter_plot.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+class ScatterPlot {
+
+    constructor( config, data ) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || {top:10, right:10, bottom:10, left:10}
+        }
+        this.data = data;
+        this.init();
+    }
+
+    init() {
+        let self = this;
+
+        self.svg = d3.select( self.config.parent )
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+        self.xscale = d3.scaleLinear()
+            .range( [0, self.inner_width] );
+
+        self.yscale = d3.scaleLinear()
+            .range( [self.inner_height,0] );
+
+
+        self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(6);
+        // Y軸の生成
+        self.yaxis = d3.axisLeft( self.yscale )
+        .ticks(6);
+
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
+
+        // Y軸グループの追加
+        self.yaxis_group = self.chart.append('g')
+        .attr('transform', `translate(0, 0)`); 
+    }
+
+    update() {
+        let self = this;
+
+        const xmin = d3.min( self.data, d => d.x );
+        const xmax = d3.max( self.data, d => d.x );
+        self.xscale.domain( [xmin, xmax] );
+
+        const ymin = d3.min( self.data, d => d.y );
+        const ymax = d3.max( self.data, d => d.y );
+        self.yscale.domain( [ymin, ymax] );
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+        self.chart.selectAll("circle")
+            .data(self.data)
+            .enter()
+            .append("circle")
+            .attr("cx", d => self.xscale( d.x ) )
+            .attr("cy", d => self.yscale( d.y ) )
+            .attr("r", d => d.r );
+
+        self.xaxis_group
+            .call( self.xaxis );
+        self.yaxis_group
+            .call( self.yaxis );
+        // X軸ラベルの追加
+        self.svg.append("text")             
+        .attr("transform",
+            "translate(" + (self.inner_width/2 + self.config.margin.left) + " ," + 
+                            (self.inner_height + self.config.margin.top + 20) + ")")
+        .style("text-anchor", "middle")
+        .text("X label"); 
+
+        // Y軸ラベルの追加
+        self.svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - self.config.margin.left)
+        .attr("x",0 - (self.inner_height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Y label"); 
+    }
+}
