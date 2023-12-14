2e14b5f4a66c7823b235bfd2248367d8eb208537
田中祐希 <tanakayuuki@tanakayuukinoMacBook-Air.local>
2023-12-14 14:59:07 +0900

    W10_commit

---
+++ W10/task1.js
@@ -0,0 +1,50 @@
+class MyD3Chart {
+    constructor(selector, csvFile) {
+        this.selector = selector;
+        this.csvFile = csvFile;
+        this.svg = d3.select(selector);
+        this.loadData();
+    }
+
+    loadData() {
+        d3.csv(this.csvFile).then(data => {
+            this.data = data.map(d => +d.value);
+            this.update(this.data);
+        });
+    }
+
+    update(data) {
+        let padding = 10;
+        let height = 20;
+        this.svg.selectAll("rect")
+            .data(data)
+            .join("rect")
+            .attr("x", padding)
+            .attr("y", (d, i) => padding + i * (height + padding))
+            .attr("width", d => d)
+            .attr("height", height);
+    }
+
+    reverseData() {
+        this.data.reverse();
+        this.update(this.data);
+    }
+
+    sortDataAscend() {
+        this.data.sort((a, b) => a - b);
+        this.update(this.data);
+    }
+
+    sortDataDescend() {
+        this.data.sort((a, b) => b - a);
+        this.update(this.data);
+    }
+}
+
+// インスタンス化とイベントハンドラの設定
+const myChart = new MyD3Chart('#drawing_region', 'https://tanakayuki19890711.github.io/InfoVis2023/W10/task1.csv');
+// const myChart = new MyD3Chart('#drawing_region', '/Users/tanakayuuki/Work/InfoVis2023/W10/task1.csv');
+
+d3.select('#reverse').on('click', () => myChart.reverseData());
+d3.select('#ascend').on('click', () => myChart.sortDataAscend());
+d3.select('#descend').on('click', () => myChart.sortDataDescend());
---
+++ W10/task2.js
@@ -0,0 +1,97 @@
+d3.csv("https://tanakayuki19890711.github.io/InfoVis2023/W10/task2.csv").then(function(data) {
+    data.forEach(function(d) {
+        d.x = +d.x; 
+        d.y = +d.y; 
+        d.r = +d.r; 
+    });
+
+    var margin = { top: 50, right: 20, bottom: 50, left: 70 };
+    var width = 500 - margin.left - margin.right;
+    var height = 400 - margin.top - margin.bottom;
+    
+    var svg = d3.select('#drawing_region')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    var xScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.x) + 10])
+        .range([0, width]);
+
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.y) + 10])
+        .range([height, 0]);
+
+    var circles = svg.selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('cx', d => xScale(d.x))
+        .attr('cy', d => yScale(d.y))
+        .attr('r', d => d.r);
+    
+    circles
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y)
+        .attr('r', d => d.r);
+    
+    circles
+        .on('mouseover', (e,d) => {
+            d3.select('#tooltip')
+                .style('opacity', 1)
+                .html(`<div class="tooltip-label">Position</div>(${d.x}, ${d.y})`);
+        })
+        .on('mousemove', (e) => {
+            const padding = 10;
+            d3.select('#tooltip')
+                .style('left', (e.pageX + padding) + 'px')
+                .style('top', (e.pageY + padding) + 'px');
+        })
+        .on('mouseleave', () => {
+            d3.select('#tooltip')
+                .style('opacity', 0);
+        });
+    
+    svg.append('g')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(d3.axisBottom(xScale));
+    
+    svg.append('g')
+        .call(d3.axisLeft(yScale));
+    
+    svg.selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('cx', d => xScale(d.x))
+        .attr('cy', d => yScale(d.y))
+        .attr('r', d => d.r);
+    
+    svg.append('text')
+        .attr('x', (width / 2))             
+        .attr('y', 0 - (margin.top / 2))
+        .attr('text-anchor', 'middle')  
+        .style('font-size', '16px') 
+        .text('Scatter Plot Title');
+    
+    svg.append('text')
+        .attr('x', width / 2 )
+        .attr('y', height + margin.bottom - 10)
+        .style('text-anchor', 'middle')
+        .text('X Axis Label');
+    
+    svg.append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 0 - margin.left)
+        .attr('x',0 - (height / 2))
+        .attr('dy', '1em')
+        .style('text-anchor', 'middle')
+        .text('Y Axis Label'); 
+
+    }).catch(function(error) {
+        console.log(error);
+    });
+    
+
+
