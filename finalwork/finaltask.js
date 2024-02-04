04c82bd3698ba7b0555f589f3d019ebbabb3348d
田中祐希 <tanakayuuki@tanakayuukinoMacBook-Air.local>
2024-02-04 15:37:25 +0900

    finalwork

--- finalwork/finaltask.js
+++ finalwork/finaltask.js
@@ -3,7 +3,7 @@ class MyLossGraph {
         this.selector = selector;
         this.csvFile = csvFile;
         this.svg = d3.select(selector);
-        this.margin = { top: 20, right: 20, bottom: 30, left: 40 };
+        this.margin = { top: 30, right: 30, bottom: 60, left: 50 };
         this.width = 960 - this.margin.left - this.margin.right;
         this.height = 500 - this.margin.top - this.margin.bottom;
         this.svg = this.svg
@@ -12,6 +12,7 @@ class MyLossGraph {
             .append("g")
             .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
         this.loadData();
+        this.maxLoss = 0;
         // 刻み幅の初期値を設定
         this.epochStep = 1; // 初期刻み幅
 
@@ -30,43 +31,87 @@ class MyLossGraph {
         }
         // グラフを更新
         this.update(this.data);
+
+        // 現在の刻み幅を表示するためのメソッドを呼び出す
+        this.updateEpochStepDisplay();
     }
     
 
     loadData() {
         d3.csv(this.csvFile).then(data => {
-            this.data = data.map((d, i) => ({x: d.epoch, y: d.loss}));
+            this.data = data.map(d => ({
+                x: +d.epoch, // `+`を使って文字列から数値へ変換
+                y: +d.loss   
+            }));
+    
+            // データセット全体のlossの最大値を計算
+            this.maxLoss = d3.max(this.data, d => +d.y); 
+    
             console.log(this.data);
+            console.log(this.maxLoss); 
+
             this.xScale = d3.scaleLinear()
                             .domain([0, this.data.length - 1])
                             .range([0, this.width]);
             this.yScale = d3.scaleLinear()
-                            .domain([0, d3.max(this.data, d => d.y)])
+                            .domain([0, this.maxLoss]) // Y軸のスケールをlossの最大値に基づいて設定
                             .range([this.height, 0]);
             this.update(this.data);
         });
     }
 
+updateEpochStepDisplay() {
+    // 現在の刻み幅を表示領域に更新
+    d3.select("#epoch-step-display").text(`現在の刻み幅: ${this.epochStep}`);
+}
+
 update(data) {
     // データをフィルタリングして刻み幅に応じたデータを表示
     data = data.filter((d, i) => i % this.epochStep === 0);
-    console.log(data);
+    // console.log(data);
 
     // xScaleのドメインを更新
     this.xScale.domain([0, d3.max(data, d => +d.x)]);
+
+    // yScaleのドメインをデータセットのlossの最大値に基づいて更新
+    this.yScale.domain([0, this.maxLoss]); // 常に全データの最大値に基づく
 
     let line = d3.line()
                  .x(d => this.xScale(+d.x)) // データのx値に基づいてスケールを適用
                  .y(d => this.yScale(+d.y));
 
     this.svg.selectAll(".line")
-        .data([data]) // d3.line expects an array of data points
+        .data([data]) 
         .join("path")
         .attr("class", "line")
         .attr("d", line)
         .attr("fill", "none")
         .attr("stroke", "steelblue")
         .attr("stroke-width", 2);
+
+    // グラフのタイトルを追加
+    this.svg.append("text")
+        .attr("x", (this.width / 2))             
+        .attr("y", 0 - (this.margin.top / 2))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "20px") 
+        .style("text-decoration", "underline")  
+        .text("Loss Value by Epoch");
+
+    // X軸のラベルを追加
+    this.svg.append("text")             
+        .attr("transform", "translate(" + (this.width/2) + " ," + (this.height + this.margin.top + 20) + ")")
+        .style("text-anchor", "middle")
+        .text("epoch");
+
+    // Y軸のラベルを追加
+    this.svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - this.margin.left)
+        .attr("x",0 - (this.height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("loss");
 
     // X軸を更新するために以前のX軸を削除
     this.svg.selectAll("g.x-axis").remove();
@@ -75,12 +120,24 @@ update(data) {
     this.svg.append("g")
         .attr("transform", "translate(0," + this.height + ")")
         .attr("class", "x-axis") // X軸のためのクラスを追加
-        .call(d3.axisBottom(this.xScale));
+        .call(d3.axisBottom(this.xScale))
 
-       // Y軸の追加
-       this.svg.append("g")
-       .call(d3.axisLeft(this.yScale));
+    // Y軸の追加
+    this.svg.append("g")
+       .call(d3.axisLeft(this.yScale))
 }
+}
+function showImage() {
+    var epoch = document.getElementById("epochInput").value; // 入力されたエポックを取得
+    var imageUrl = getImageUrlForEpoch(epoch); // エポックに基づいた画像のURLを取得する関数（実装が必要）
+    var imageDisplay = document.getElementById("imageDisplay"); // 画像を表示する要素を取得
+    imageDisplay.innerHTML = `<img src="${imageUrl}" alt="Image for epoch ${epoch}">`; // 画像を表示
+}
+
+function getImageUrlForEpoch(epoch) {
+    // ここでエポックに対応する画像のURLを返すロジックを実装します。
+    // 例えば、画像が特定のパターンでURLが設定されている場合、そのパターンに基づいてURLを構築できます。
+    return `https://tanakayuki19890711.github.io/InfoVis2023/finalwork/${epoch}.png`; // 仮のURL構築例
 }
 const myLossGraph = new MyLossGraph('#drawing_region', 'https://tanakayuki19890711.github.io/InfoVis2023/finalwork/loss.csv');
 // const myLossGraph = new MyLossGraph('#drawing_region', '/Users/tanakayuuki/Work/InfoVis2023/finalwork/loss.csv');
